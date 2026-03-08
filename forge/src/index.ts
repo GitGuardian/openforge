89f2b6cbe10283c96569c06750bfe464b381e324
Jeremy Brown <jeremy.brown@gitguardian.com>
2026-03-08 11:18:05 +0100

    feat(forge): add CSRF middleware

--- forge/src/index.ts
+++ forge/src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
+import { csrf } from "hono/csrf";
 import { logger } from "hono/logger";
 import { authMiddleware } from "./middleware/auth";
 import { healthRoutes } from "./routes/health";
@@ -12,6 +13,7 @@ const app = new Hono<AppEnv>();
 
 // Middleware
 app.use("*", logger());
+app.use("*", csrf());
 app.use("*", authMiddleware);
 
 // Static files
